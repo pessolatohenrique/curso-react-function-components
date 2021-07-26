1c99676f58317be713428e688321b2a5e35af246
Henrique Pessolato <pessolatohenrique@gmail.com>
2021-07-26 19:14:03 -0300

    Config inicial do material-ui

---
+++ src/components/FormRegister.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container, Button } from "@material-ui/core";
+
+function FormRegister() {
+  return (
+    <Container maxWidth="sm">
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    </Container>
+  );
+}
+
+export default FormRegister;
