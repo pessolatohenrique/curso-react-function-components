96b1c110d8145b38a9018c493d4c0f13a90ea1d5
Henrique Pessolato <pessolatohenrique@gmail.com>
2021-07-28 16:12:55 -0300

    Construcao do formulario base com material-ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import { Fragment } from "react";
+import { Container, Typography } from "@material-ui/core";
 import FormRegister from "./components/FormRegister";
+import "@fontsource/roboto";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    marginTop: "20px",
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <Fragment>
+    <Container maxWidth="md" className={classes.container}>
+      <Typography component="h1" variant="h3" align="center" color="primary">
+        Cadastre-se
+      </Typography>
       <FormRegister />
-    </Fragment>
+    </Container>
   );
 }
 
--- src/components/FormRegister.js
+++ src/components/FormRegister.js
@@ -1,13 +1,44 @@
 import React from "react";
-import { Container, Button } from "@material-ui/core";
+import { TextField, Button, FormControlLabel, Switch } from "@material-ui/core";
 
 function FormRegister() {
   return (
-    <Container maxWidth="sm">
+    <form>
+      <TextField
+        id="name"
+        label="Nome"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+      />
+      <TextField
+        id="lastname"
+        label="Sobrenome"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+      />
+      <TextField
+        id="cpf"
+        label="CPF"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+      />
+      <FormControlLabel
+        control={<Switch name="sales" color="primary" checked />}
+        label="Promoções"
+      />
+      <FormControlLabel
+        control={<Switch name="news" color="primary" checked />}
+        label="Novidades"
+      />
+      <br />
+      <br />
       <Button variant="contained" color="primary">
-        Hello World
+        Salvar
       </Button>
-    </Container>
+    </form>
   );
 }
 
